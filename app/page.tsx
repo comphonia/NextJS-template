8d5271f3adcd8101e4bd70915b9a3a733c6cbfb4
comphonia <37687869+comphonia@users.noreply.github.com>
2025-01-03 03:44:10 -0600

    Added lint staged

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ import { Button } from "../components/atoms/button";
 
 export default function Home() {
 	return (
-		<div className="flex flex-col items-center justify-center min-h-screen py-2">
+		<div className="flex min-h-screen flex-col items-center justify-center py-2">
 			<Image alt="Vercel logo" className="h-12" height={96} src="/vercel.svg" width={96} />
-			<h1 className="text-6xl font-bold mb-8">Hello, world!</h1>
+			<h1 className="mb-8 text-6xl font-bold">Hello, world!</h1>
 			<Button size="lg">Click me</Button>
 		</div>
 	);
