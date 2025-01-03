dc5199d9a81b0e981916e6df737740423782824a
comphonia <37687869+comphonia@users.noreply.github.com>
2025-01-03 03:05:56 -0600

    Setup  prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,18 +2,18 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Everistus Akpabio",
-  description: "A full-stack developer with a passion for building scalable and maintainable software.",
+	title: "Everistus Akpabio",
+	description: "A full-stack developer with a passion for building scalable and maintainable software.",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" className="dark">
-      <body>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="en" className="dark">
+			<body>{children}</body>
+		</html>
+	);
 }
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,14 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier"
+  ),
 ];
 
 export default eslintConfig;
