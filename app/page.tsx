a80a879ff397837cce6cc0d12fac9b783906abff
comphonia <37687869+comphonia@users.noreply.github.com>
2025-01-03 03:23:13 -0600

    Setup prettier

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,11 @@ import Image from "next/image";
 import { Button } from "../components/atoms/button";
 
 export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Image alt="Vercel logo" className="h-12" height={96} src="/vercel.svg" width={96} />
-      <h1 className="text-6xl font-bold mb-8">Hello, world!</h1>
-      <Button size="lg">Click me</Button>
-    </div>
-  );
+	return (
+		<div className="flex flex-col items-center justify-center min-h-screen py-2">
+			<Image alt="Vercel logo" className="h-12" height={96} src="/vercel.svg" width={96} />
+			<h1 className="text-6xl font-bold mb-8">Hello, world!</h1>
+			<Button size="lg">Click me</Button>
+		</div>
+	);
 }
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,18 +6,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+	baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends(
-    "next/core-web-vitals",
-    "next/typescript",
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier"
-  ),
-];
+const eslintConfig = [...compat.extends("next/core-web-vitals", "next/typescript", "prettier")];
 
 export default eslintConfig;
